003d4dfadcf4cf063f436a1d58a26ec5ea372780
Alessandro Volpe <37452470+Syro98@users.noreply.github.com>
2024-03-06 20:54:53 +0200

    Implementing interface

---
+++ id_generation.c
@@ -0,0 +1,24 @@
+//
+// Created by aless on 06/03/2024.
+//
+
+#include "headers/id_generation.h"
+
+#include <stdlib.h>
+#include <time.h>
+
+char* generate_uuid() {
+    const char v[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    static char buf[37] = {0};
+    for (int i = 0; i < 36; i++) {
+        srand(time(NULL));
+        buf[i] = v[rand()%16];
+    }
+
+    buf[8] = '-';
+    buf[13] = '-';
+    buf[18] = '-';
+    buf[23] = '-';
+    buf[36] = '\0';
+    return buf;
+}
