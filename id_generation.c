276d777daf51f6bba7ab46a6f4254ff3ff307dbe
Alessandro Volpe <37452470+Syro98@users.noreply.github.com>
2024-03-06 21:41:37 +0200

    removed unused import

--- id_generation.c
+++ id_generation.c
@@ -5,7 +5,6 @@
 #include "headers/id_generation.h"
 
 #include <stdlib.h>
-#include <time.h>
 
 char* generate_uuid() {
     const char v[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
