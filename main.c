6f8ee42a1ccdea5bb9e92bb59e68a71995c2f403
Alessandro Volpe <37452470+AlessVolpe@users.noreply.github.com>
2024-04-08 11:30:30 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// #include "bostree.h"
+#include "bostree.h"
 #include "commons.h"
 #include "hashmap.h"
 #include "hashmap_base.h"
@@ -9,10 +9,10 @@
 #include "player.h"
 
 
-// static void _tree_free(const BOSNode* node) {
-//     free(node->key);
-//     free(node->data);
-// }
+static void _tree_free(const BOSNode* node) {
+    free(node->key);
+    free(node->data);
+}
 
 static int _select_players(void) {
     int n;
@@ -23,6 +23,9 @@ static int _select_players(void) {
     return n;
 }
 
+/*
+ * TODO: fix memory leaks
+ */
 static char** _name_players(const int num) {
     char** names = malloc(num * sizeof(char*));
     for (int i = 0; i < num; i++) {
@@ -51,10 +54,16 @@ static void _free_player_names(const int num, char** name_arr) {
     free(name_arr);
 }
 
+static void _cleanup(player_t* player_ptr, char* id_ptr, char** names_array, const size_t arr_size) {
+    if (player_ptr) free(player_ptr);
+    if (id_ptr) free(id_ptr);
+    if (names_array) _free_player_names(arr_size, names_array);
+}
+
 int main(int argc, char** argv) {
     char* player_id = NULL;
     player_t* player = NULL;
-    const int player_num = _select_players();
+    const size_t player_num = _select_players();
     char** player_names = _name_players(player_num);
     printf("\n");
 
@@ -62,7 +71,7 @@ int main(int argc, char** argv) {
     HASHMAP(char, player_t) pool;
     char* key;
     player_t* data;
-    // void* temp;
+    void* temp;
 
     // initialize with default string key hash function and comparator
     hashmap_init(&pool, hashmap_hash_string, strcmp);
@@ -83,27 +92,25 @@ int main(int argc, char** argv) {
         player_adding_points(data, (uint32_t)(genRand(seed) * 100)); // adding a random ammount of points
         printf("Player: %s[%s] with score %d\n", data->name, key, data->score);
     }
-//     printf("\n");
-//
-//     BOSTree* leaderboard = bostree_new((BOSTree_cmp_func)strcmp, _tree_free);
-//
-//     hashmap_foreach_key_safe(key, &pool, temp) {
-//         const player_t* data_ptr = hashmap_remove(&pool, key);
-//         char* tr_key = data_ptr->name;
-//         bostree_insert(leaderboard, tr_key, (void*)data_ptr->score);
-//         printf("Inserted player %s with %d points.\n", tr_key, data_ptr->score);
-//     }
-//
-// #ifndef NDEBUG
-//     bostree_print(leaderboard);
-// #endif
+    printf("\n");
+
+    BOSTree* leaderboard = bostree_new((BOSTree_cmp_func)strcmp, _tree_free);
+
+    hashmap_foreach_key_safe(key, &pool, temp) {
+        const player_t* data_ptr = hashmap_remove(&pool, key);
+        char* tr_key = data_ptr->name;
+        bostree_insert(leaderboard, tr_key, (void*)data_ptr->score);
+        printf("Inserted player %s with %d points.\n", tr_key, data_ptr->score);
+    }
+
+#ifndef NDEBUG
+    bostree_print(leaderboard);
+#endif
 
     // cleanup
-    if (player) free(player);
-    if (player_id) free(player_id);
-    if (player_names) _free_player_names(player_num, player_names);
     hashmap_cleanup(&pool);
-    // bostree_destroy(leaderboard);
+    bostree_destroy(leaderboard);
+    _cleanup(player, player_id, player_names, player_num);
 
     exit(EXIT_SUCCESS);
 }
